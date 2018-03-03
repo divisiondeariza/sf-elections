d46af520ed133de25d5b5a179cc4cc85e8350865
Emmanuel Ariza <emmanuel@emmanuel.emmanuel-pc>
2018-03-03 12:04:51 -0500

    created start page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule,
